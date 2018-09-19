0bcae9d190390139ca8fb54cd50b17361c9ef55e
sairamaj <sairamaj@gmail.com>
2018-09-18 19:55:54 -0700

    Initial commit

---
+++ Common/ViewModels/CoreViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Common.ViewModels
+{
+	public class CoreViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged<T>(Expression<Func<T>> selector)
+		{
+			if( selector == null)
+			{
+				throw new ArgumentNullException("selector");
+			}
+
+			var body = selector.Body as MemberExpression;
+			if( body == null)
+			{
+				throw new ArgumentException("Given expression is not of type" + typeof(MemberExpression));
+			}
+
+			var propertyName = body.Member.Name;
+			if( this.PropertyChanged != null)
+			{ 
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
---
+++ HelpApp/ViewModel/RootTreeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpApp.ViewModel
+{
+	public class RootTreeViewModel : TreeViewItemViewModel
+	{
+		public RootTreeViewModel(string name) : base(null, true)
+		{
+			this.Name = name;
+		}
+
+		public string Name { get; set; }
+	}
+}
